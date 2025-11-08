acc8221fab2cd443d84e452f7db881933fb5b14a
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-11-08 10:22:50 +0600

    now i can handle conditions

---
+++ day2/condition.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var score int
+
+	fmt.Println("Please input number : ")
+	fmt.Scan(&score)
+
+	// fmt.Printf("Socre is : %d\n, Type: %T\n", score, score)
+
+	// Handle the input error
+	if score < 0 || score > 100 {
+		fmt.Print("Score can't less then 0 and grater then 100")
+	}
+
+	// Print the results
+	if score >= 80 {
+		fmt.Println("Result is : Pass (A+)")
+	} else if score >= 70 {
+		fmt.Println("Result is Pass (A)")
+	} else if score >= 60 {
+		fmt.Println("Result is Pass (B)")
+	} else if score >= 50 {
+		fmt.Println("Result is Pass (C)")
+	} else if score >= 33 {
+		fmt.Println("Result is Pass (D)")
+	} else {
+		fmt.Println("Result is fail")
+	}
+}
--- day2/nestedObject.go
+++ day2/nestedObject.go
@@ -24,37 +24,37 @@ type School struct {
 	Students   []Student
 }
 
-func main() {
-	person1 := Student{
-		Name:     "Akash",
-		Age:      24,
-		Address:  Address{"Pabna", "Faridpur", 8, 77},
-		IsPassed: true,
-		GPA:      4.44,
-		Roll:     12,
-	}
-	person2 := Student{
-		Name:     "Atik",
-		Age:      23,
-		Address:  Address{"Pabna", "Faridpur", 8, 77},
-		IsPassed: true,
-		GPA:      4.45,
-		Roll:     10,
-	}
-	person3 := Student{
-		Name:     "Bataws",
-		Age:      24,
-		Address:  Address{"Pabna", "Faridpur", 8, 77},
-		IsPassed: false,
-		GPA:      0,
-		Roll:     19,
-	}
+// func main() {
+// 	person1 := Student{
+// 		Name:     "Akash",
+// 		Age:      24,
+// 		Address:  Address{"Pabna", "Faridpur", 8, 77},
+// 		IsPassed: true,
+// 		GPA:      4.44,
+// 		Roll:     12,
+// 	}
+// 	person2 := Student{
+// 		Name:     "Atik",
+// 		Age:      23,
+// 		Address:  Address{"Pabna", "Faridpur", 8, 77},
+// 		IsPassed: true,
+// 		GPA:      4.45,
+// 		Roll:     10,
+// 	}
+// 	person3 := Student{
+// 		Name:     "Bataws",
+// 		Age:      24,
+// 		Address:  Address{"Pabna", "Faridpur", 8, 77},
+// 		IsPassed: false,
+// 		GPA:      0,
+// 		Roll:     19,
+// 	}
 
-	school1 := School{
-		SchoolName: "Sky Codding School",
-		Date:       "12/12/12",
-		Students:   []Student{person1, person2, person3},
-	}
+// 	school1 := School{
+// 		SchoolName: "Sky Codding School",
+// 		Date:       "12/12/12",
+// 		Students:   []Student{person1, person2, person3},
+// 	}
 
-	fmt.Print(school1)
-}
+// 	fmt.Print(school1)
+// }
