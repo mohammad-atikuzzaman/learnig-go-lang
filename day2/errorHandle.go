131eb2bc209eddc64c18c9f37b68a23f61baab89
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-11-08 10:39:48 +0600

    now I can handle error

---
+++ day2/errorHandle.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func calCulateCircleArea(radius float64) (float64, error) {
+	if radius < 0 {
+		return 0, fmt.Errorf("Radius cant be negative")
+	}
+
+	return math.Pi * radius * radius, nil
+}
+
+func calculateTriangleArea(base float64, hight float64) (float64, error) {
+	if base <= 0 || hight <= 0 {
+		return 0, fmt.Errorf("Base or hight cant be 0 or less then 0")
+	}
+
+	return 0.5 * base * hight, nil
+}
+
+
+func main(){
+	// area, error := calCulateCircleArea(10)
+	area, error := calculateTriangleArea(10, 20)
+	if(error != nil){
+		fmt.Println(error)
+	}else{
+		fmt.Println(area)
+	}
+}
