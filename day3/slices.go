4c8e5f9df1623eec943982402e45749efac46854
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-11-14 11:10:44 +0600

    learning slice in progress

--- day3/slices.go
+++ day3/slices.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	// declaration of slices
-	var slice1 []int
-	fmt.Println(slice1)
+	var slice1 []string
+	fmt.Println(slice1)  // []
+
+	slice2 := []int{1,2,3,4,5}
+	fmt.Println(slice2)
 }
