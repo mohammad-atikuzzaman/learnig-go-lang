b6bdef237a265d633a088e328680e9e6b939b9ad
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-11-24 10:05:07 +0600

    I have complted basic file operations

--- day3/fileOperations/besic.go
+++ day3/fileOperations/besic.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -24,4 +25,44 @@ func main() {
 	}
 	// the data of file is in byte format, so we have convert byte in string
 	fmt.Println("This file content is : ", string(data))
+
+	// Reading line by line
+	file, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println("Unable to open file", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNumber := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf("Line %d: %s\n", lineNumber, line)
+		lineNumber++
+	}
+
+	// appending to file
+
+	appendText := "\nThis is append text content."
+	f, err := os.OpenFile("test.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Unable to open file", err)
+		return
+	}
+	defer f.Close()
+	if _, err := f.WriteString(appendText); err != nil {
+		fmt.Println("Error appending to file: ", err)
+		return
+	}
+	fmt.Println("Content append successful")
+
+	// Check if file exists
+	if _, err := os.Stat("test.txt"); err == nil {
+		fmt.Println("File exists!")
+	} else if os.IsNotExist(err) {
+		fmt.Println("File does not exist!")
+	} else {
+		fmt.Println("Error checking file:", err)
+	}
 }
